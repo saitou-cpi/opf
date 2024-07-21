43200cbb5812ac5d8d414f247e31bce2c070d905
saitou-cpi <odeco_de_odeco@yahoo.co.jp>
2024-07-21 21:19:41 +0900

    複数の証券コードに対応

--- get_stock_month.py
+++ get_stock_month.py
@@ -4,58 +4,57 @@ import datetime
 import os
 from time import sleep
 
-from config.vars import ticker_symbol
-
-# 証券コードを設定します。Yahoo Financeでは日本の証券コードに .T を付けます。
-ticker_symbol = ticker_symbol
-
-# データを格納するリストを初期化
-all_data = []
-
-# 現在の日付から開始
-current_date = datetime.datetime.now()
-# 7日分のデータを取得
-delta = datetime.timedelta(days=7)
-
-# 過去1ヶ月分のデータを取得するためのループ
-for _ in range(4):  # 1ヶ月は約4週間と仮定
-    end_date = current_date
-    start_date = end_date - delta
-
-    # データをダウンロード
-    try:
-        data = yf.download(ticker_symbol,
-                           start=start_date.strftime('%Y-%m-%d'),
-                           end=end_date.strftime('%Y-%m-%d'),
-                           interval="1d"  # 日足データ
-                           )
-        all_data.append(data)
-    except Exception as e:
-        print(f"Error downloading data for {start_date} to {end_date}: {e}")
-
-    # 次の期間に移動
-    current_date = start_date
-
-    # API制限を避けるために少し待機
-    sleep(2)  # 2秒待機
-
-# 全てのデータを連結
-full_data = pd.concat(all_data)
-
-# 日付順に並び替え
-full_data.sort_index(inplace=True)
+from config.vars import ticker_symbols
 
 # ディレクトリの作成
 output_dir = "stockdata"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-# 現在の日付を取得してフォーマット
-date_str = datetime.datetime.now().strftime('%Y%m%d')
+# 各証券コードについてデータを取得
+for ticker_symbol in ticker_symbols:
+    # データを格納するリストを初期化
+    all_data = []
 
-# データをCSV形式で保存
-csv_filename = os.path.join(output_dir,
-                            f'{ticker_symbol.replace(".", "_")}_one_month_daily_stock_data_{date_str}.csv')
-full_data.to_csv(csv_filename)
+    # 現在の日付から開始
+    current_date = datetime.datetime.now()
+    # 7日分のデータを取得
+    delta = datetime.timedelta(days=7)
 
-print(f"日足の株価データをCSV形式で {csv_filename} に保存しました。")
+    # 過去1ヶ月分のデータを取得するためのループ
+    for _ in range(4):  # 1ヶ月は約4週間と仮定
+        end_date = current_date
+        start_date = end_date - delta
+
+        # データをダウンロード
+        try:
+            data = yf.download(ticker_symbol,
+                               start=start_date.strftime('%Y-%m-%d'),
+                               end=end_date.strftime('%Y-%m-%d'),
+                               interval="1d"  # 日足データ
+                               )
+            all_data.append(data)
+        except Exception as e:
+            print(f"Error downloading data for {start_date} to {end_date}: {e}")
+
+        # 次の期間に移動
+        current_date = start_date
+
+        # API制限を避けるために少し待機
+        sleep(2)  # 2秒待機
+
+    # 全てのデータを連結
+    full_data = pd.concat(all_data)
+
+    # 日付順に並び替え
+    full_data.sort_index(inplace=True)
+
+    # 現在の日付を取得してフォーマット
+    date_str = datetime.datetime.now().strftime('%Y%m%d')
+
+    # データをCSV形式で保存
+    csv_filename = os.path.join(output_dir,
+                                f'{ticker_symbol.replace(".", "_")}_one_month_daily_stock_data_{date_str}.csv')
+    full_data.to_csv(csv_filename)
+
+    print(f"日足の株価データをCSV形式で {csv_filename} に保存しました。")
--- optimal_parameter_finder.py
+++ optimal_parameter_finder.py
@@ -3,20 +3,21 @@ import datetime
 import pandas as pd
 import logging
 import numpy as np
-from config.vars import ticker_symbol, initial_capital
+from config.vars import ticker_symbols, initial_capital, upper_limits, \
+    lower_limits
+
 
 # ログの設定
 def setup_logging(symbol):
     log_dir = os.path.abspath("optimal_parameter_log")
     os.makedirs(log_dir, exist_ok=True)
     results_date_str = datetime.datetime.now().strftime('%Y%m%d%H%M')
-    log_filename = os.path.join(log_dir, f'{symbol.replace(".", "_")}_optimal_parameter_{results_date_str}.log')
+    log_filename = os.path.join(log_dir,
+                                f'{symbol.replace(".", "_")}_optimal_parameter_{results_date_str}.log')
     logging.basicConfig(filename=log_filename, level=logging.INFO)
     return log_dir
 
-log_dir = setup_logging(ticker_symbol)
 
-# 株価データの読み込み
 def load_stock_data(filename):
     if os.path.exists(filename):
         df = pd.read_csv(filename, index_col='Date', parse_dates=True)
@@ -27,10 +28,6 @@ def load_stock_data(filename):
         logging.error(f"File {filename} does not exist")
         raise FileNotFoundError(f"File {filename} does not exist")
 
-output_dir = os.path.abspath("stockdata")
-stockdata_date_str = datetime.datetime.now().strftime('%Y%m%d')
-csv_filename = os.path.join(output_dir, f'{ticker_symbol.replace(".", "_")}_one_month_daily_stock_data_{stockdata_date_str}.csv')
-df = load_stock_data(csv_filename)
 
 class TradeModel:
     def __init__(self, initial_capital):
@@ -43,8 +40,11 @@ class TradeModel:
         if quantity > 0:
             self.capital -= quantity * price
             self.holding_quantity += quantity
-            self.average_price = ((self.average_price * (self.holding_quantity - quantity)) + (price * quantity)) / self.holding_quantity
-            logging.info(f"Bought {quantity} shares at {price} each. New capital: {self.capital}, Holding: {self.holding_quantity}, Average purchase price: {self.average_price}")
+            self.average_price = ((self.average_price * (
+                        self.holding_quantity - quantity)) + (
+                                              price * quantity)) / self.holding_quantity
+            logging.info(
+                f"Bought {quantity} shares at {price} each. New capital: {self.capital}, Holding: {self.holding_quantity}, Average purchase price: {self.average_price}")
 
     def sell_stock(self, price, quantity):
         quantity = min(quantity, self.holding_quantity)
@@ -53,7 +53,9 @@ class TradeModel:
             self.holding_quantity -= quantity
             if self.holding_quantity == 0:
                 self.average_price = 0
-            logging.info(f"Sold {quantity} shares at {price} each. New capital: {self.capital}, Holding: {self.holding_quantity}, Average purchase price: {self.average_price}")
+            logging.info(
+                f"Sold {quantity} shares at {price} each. New capital: {self.capital}, Holding: {self.holding_quantity}, Average purchase price: {self.average_price}")
+
 
 class TradeController:
     def __init__(self, df, symbol, initial_capital):
@@ -74,84 +76,116 @@ class TradeController:
         if len(prices) < window:
             return None
         moving_average = np.convolve(prices, np.ones(window), 'valid') / window
-        self.logger.info(f"Calculated moving average for window {window}: {moving_average}")
+        self.logger.info(
+            f"Calculated moving average for window {window}: {moving_average}")
         return moving_average
 
     def trading_logic(self, current_price, upper_limit, lower_limit):
         action, quantity = None, 0
 
         if len(self.historical_prices) < 10:
-            self.logger.error("Not enough historical data to calculate moving averages.")
+            self.logger.error(
+                "Not enough historical data to calculate moving averages.")
             return action, quantity
 
         short_term_ma = self.calculate_moving_average(self.historical_prices, 5)
         long_term_ma = self.calculate_moving_average(self.historical_prices, 10)
 
-        if any(x is None for x in [short_term_ma, long_term_ma]) or any(len(x) == 0 for x in [short_term_ma, long_term_ma]):
+        if any(x is None for x in [short_term_ma, long_term_ma]) or any(
+                len(x) == 0 for x in [short_term_ma, long_term_ma]):
             self.logger.error("Error calculating moving averages.")
             return action, quantity
 
         min_length = min(len(short_term_ma), len(long_term_ma))
-        short_term_ma, long_term_ma = short_term_ma[-min_length:], long_term_ma[-min_length:]
+        short_term_ma, long_term_ma = short_term_ma[-min_length:], long_term_ma[
+                                                                   -min_length:]
 
-        self.logger.info(f"Short-term MA: {short_term_ma[-1]}, Long-term MA: {long_term_ma[-1]}")
-        self.logger.info(f"Before Action - Capital: {self.model.capital}, Holding Quantity: {self.model.holding_quantity}, Average Price: {self.model.average_price}")
+        self.logger.info(
+            f"Short-term MA: {short_term_ma[-1]}, Long-term MA: {long_term_ma[-1]}")
+        self.logger.info(
+            f"Before Action - Capital: {self.model.capital}, Holding Quantity: {self.model.holding_quantity}, Average Price: {self.model.average_price}")
 
         if self.model.holding_quantity == 0:
             quantity = int(self.model.capital / current_price)
             if quantity > 0:
                 action = 'buy'
-                self.model.buy_stock(current_price, quantity)
         else:
-            if current_price >= self.model.average_price * upper_limit and short_term_ma[-1] > long_term_ma[-1]:
+            if current_price >= self.model.average_price * upper_limit and \
+                    short_term_ma[-1] > long_term_ma[-1]:
                 action, quantity = 'sell', self.model.holding_quantity
-                self.model.sell_stock(current_price, quantity)
             elif current_price <= self.model.average_price * lower_limit:
                 action, quantity = 'sell', self.model.holding_quantity
-                self.model.sell_stock(current_price, quantity)
 
-        self.logger.info(f"After Action - Capital: {self.model.capital}, Holding Quantity: {self.model.holding_quantity}, Average Price: {self.model.average_price}")
-        self.logger.info(f"Action: {action}, Quantity: {quantity}, Price: {current_price}")
+        self.logger.info(
+            f"After Action - Capital: {self.model.capital}, Holding Quantity: {self.model.holding_quantity}, Average Price: {self.model.average_price}")
+        self.logger.info(
+            f"Action: {action}, Quantity: {quantity}, Price: {current_price}")
         return action, quantity
 
-def optimize_parameters(df, upper_limit, lower_limit):
+
+def optimize_parameters(df, upper_limit, lower_limit, ticker_symbol):
     trade_controller = TradeController(df, ticker_symbol, initial_capital)
 
     for price in df['Close']:
         logging.info(f"Current price: {price}")
-        action, quantity = trade_controller.trading_logic(price, upper_limit, lower_limit)
+        action, quantity = trade_controller.trading_logic(price, upper_limit,
+                                                          lower_limit)
 
-    final_value = trade_controller.model.capital + trade_controller.model.holding_quantity * df.iloc[-1]['Close']
+        # action に基づいてトレードを実行
+        if action == 'buy':
+            trade_controller.model.buy_stock(price, quantity)
+        elif action == 'sell':
+            trade_controller.model.sell_stock(price, quantity)
+
+    final_value = trade_controller.model.capital + trade_controller.model.holding_quantity * \
+                  df.iloc[-1]['Close']
     profit_loss = final_value - initial_capital
     return final_value, profit_loss
 
+
 def main():
-    param_combinations = [(ul, ll) for ul in [1.01, 1.05, 1.10, 1.15, 1.20] for ll in [0.90, 0.95, 0.97, 0.99, 1.00]]
+    for ticker_symbol in ticker_symbols:
+        log_dir = setup_logging(ticker_symbol)
+        stockdata_date_str = datetime.datetime.now().strftime('%Y%m%d')
+        csv_filename = os.path.join("stockdata",
+                                    f'{ticker_symbol.replace(".", "_")}_one_month_daily_stock_data_{stockdata_date_str}.csv')
+        df = load_stock_data(csv_filename)
 
-    best_upper_limit, best_lower_limit = None, None
-    best_profit_loss = float('-inf')
-    results = []
+        param_combinations = [(ul, ll) for ul in upper_limits for ll in
+                              lower_limits]
 
-    for upper_limit, lower_limit in param_combinations:
-        final_value, profit_loss = optimize_parameters(df, upper_limit, lower_limit)
-        results.append((upper_limit, lower_limit, final_value, profit_loss))
-        if profit_loss > best_profit_loss:
-            best_upper_limit = upper_limit
-            best_lower_limit = lower_limit
-            best_profit_loss = profit_loss
+        best_upper_limit, best_lower_limit = None, None
+        best_profit_loss = float('-inf')
+        results = []
 
-        logging.info(f"Upper limit: {upper_limit}, Lower limit: {lower_limit}, Final value: {final_value}, Profit/Loss: {profit_loss}")
+        for upper_limit, lower_limit in param_combinations:
+            final_value, profit_loss = optimize_parameters(df, upper_limit,
+                                                           lower_limit,
+                                                           ticker_symbol)
+            results.append((upper_limit, lower_limit, final_value, profit_loss))
+            if profit_loss > best_profit_loss:
+                best_upper_limit = upper_limit
+                best_lower_limit = lower_limit
+                best_profit_loss = profit_loss
 
-    print(f"Best upper limit: {best_upper_limit}")
-    print(f"Best lower limit: {best_lower_limit}")
-    print(f"Best Profit/Loss: {best_profit_loss}")
+            logging.info(
+                f"Upper limit: {upper_limit}, Lower limit: {lower_limit}, Final value: {final_value}, Profit/Loss: {profit_loss}")
 
-    # 結果をCSVに保存
-    results_df = pd.DataFrame(results, columns=['upper_limit', 'lower_limit', 'final_value', 'profit_loss'])
-    results_filename = os.path.join(log_dir, f'{ticker_symbol.replace(".", "_")}_optimal_parameters_results_{datetime.datetime.now().strftime("%Y%m%d%H%M")}.csv')
-    os.makedirs(os.path.dirname(results_filename), exist_ok=True)
-    results_df.to_csv(results_filename, index=False)
-    logging.info(f"Backtest results saved to {results_filename}")
+        print(f"Ticker: {ticker_symbol}")
+        print(f"Best upper limit: {best_upper_limit}")
+        print(f"Best lower limit: {best_lower_limit}")
+        print(f"Best Profit/Loss: {best_profit_loss}")
+
+        # 結果をCSVに保存
+        results_df = pd.DataFrame(results,
+                                  columns=['upper_limit', 'lower_limit',
+                                           'final_value', 'profit_loss'])
+        results_filename = os.path.join(log_dir,
+                                        f'{ticker_symbol.replace(".", "_")}_optimal_parameters_results_{datetime.datetime.now().strftime("%Y%m%d%H%M")}.csv')
+        os.makedirs(os.path.dirname(results_filename), exist_ok=True)
+        results_df.to_csv(results_filename, index=False)
+        logging.info(f"Backtest results saved to {results_filename}")
+
 
 if __name__ == "__main__":
     main()
