952f5d82bfa23138586030f627381d00d028deb5
saitou-cpi <115441742+saitou-cpi@users.noreply.github.com>
2024-07-23 13:11:56 +0900

    直近のトレンドの出力を実装

--- optimal_parameter_finder.py
+++ optimal_parameter_finder.py
@@ -3,8 +3,7 @@ import datetime
 import pandas as pd
 import logging
 import numpy as np
-from config.vars import ticker_symbols, initial_capital, upper_limits, \
-    lower_limits
+from config.vars import ticker_symbols, initial_capital, upper_limits, lower_limits
 
 
 # ログの設定
@@ -12,8 +11,7 @@ def setup_logging(symbol):
     log_dir = os.path.abspath("optimal_parameter_log")
     os.makedirs(log_dir, exist_ok=True)
     results_date_str = datetime.datetime.now().strftime('%Y%m%d%H%M')
-    log_filename = os.path.join(log_dir,
-                                f'{symbol.replace(".", "_")}_optimal_parameter_{results_date_str}.log')
+    log_filename = os.path.join(log_dir, f'{symbol.replace(".", "_")}_optimal_parameter_{results_date_str}.log')
     logging.basicConfig(filename=log_filename, level=logging.INFO)
     return log_dir
 
@@ -36,27 +34,21 @@ class TradeModel:
         self.average_price = 0
 
     def buy_stock(self, price, quantity):
-        quantity = min(quantity,
-                       (self.capital // price) // 100 * 100)  # 100株単位に調整
+        quantity = min(quantity, (self.capital // price) // 100 * 100)  # 100株単位に調整
         if quantity > 0:
             self.capital -= quantity * price
             self.holding_quantity += quantity
-            self.average_price = ((self.average_price * (
-                        self.holding_quantity - quantity)) + (
-                                              price * quantity)) / self.holding_quantity
-            logging.info(
-                f"Bought {quantity} shares at {price} each. New capital: {self.capital}, Holding: {self.holding_quantity}, Average purchase price: {self.average_price}")
+            self.average_price = ((self.average_price * (self.holding_quantity - quantity)) + (price * quantity)) / self.holding_quantity
+            logging.info(f"Bought {quantity} shares at {price} each. New capital: {self.capital}, Holding: {self.holding_quantity}, Average purchase price: {self.average_price}")
 
     def sell_stock(self, price, quantity):
-        quantity = min(quantity,
-                       (self.holding_quantity // 100) * 100)  # 100株単位に調整
+        quantity = min(quantity, (self.holding_quantity // 100) * 100)  # 100株単位に調整
         if quantity > 0:
             self.capital += quantity * price
             self.holding_quantity -= quantity
             if self.holding_quantity == 0:
                 self.average_price = 0
-            logging.info(
-                f"Sold {quantity} shares at {price} each. New capital: {self.capital}, Holding: {self.holding_quantity}, Average purchase price: {self.average_price}")
+            logging.info(f"Sold {quantity} shares at {price} each. New capital: {self.capital}, Holding: {self.holding_quantity}, Average purchase price: {self.average_price}")
 
 
 class TradeController:
@@ -78,115 +70,111 @@ class TradeController:
         if len(prices) < window:
             return None
         moving_average = np.convolve(prices, np.ones(window), 'valid') / window
-        self.logger.info(
-            f"Calculated moving average for window {window}: {moving_average}")
+        self.logger.info(f"Calculated moving average for window {window}: {moving_average}")
         return moving_average
 
     def trading_logic(self, current_price, upper_limit, lower_limit):
         action, quantity = None, 0
 
-        if len(self.historical_prices) < 15:
-            self.logger.error(
-                "Not enough historical data to calculate moving averages.")
+        if len(self.historical_prices) < 10:
+            self.logger.error("Not enough historical data to calculate moving averages.")
             return action, quantity
 
         short_term_ma = self.calculate_moving_average(self.historical_prices, 5)
-        long_term_ma = self.calculate_moving_average(self.historical_prices, 15)
+        long_term_ma = self.calculate_moving_average(self.historical_prices, 10)
 
-        if any(x is None for x in [short_term_ma, long_term_ma]) or any(
-                len(x) == 0 for x in [short_term_ma, long_term_ma]):
+        if any(x is None for x in [short_term_ma, long_term_ma]) or any(len(x) == 0 for x in [short_term_ma, long_term_ma]):
             self.logger.error("Error calculating moving averages.")
             return action, quantity
 
         min_length = min(len(short_term_ma), len(long_term_ma))
-        short_term_ma, long_term_ma = short_term_ma[-min_length:], long_term_ma[
-                                                                   -min_length:]
+        short_term_ma, long_term_ma = short_term_ma[-min_length:], long_term_ma[-min_length:]
 
-        self.logger.info(
-            f"Short-term MA: {short_term_ma[-1]}, Long-term MA: {long_term_ma[-1]}")
-        self.logger.info(
-            f"Before Action - Capital: {self.model.capital}, Holding Quantity: {self.model.holding_quantity}, Average Price: {self.model.average_price}")
+        self.logger.info(f"Short-term MA: {short_term_ma[-1]}, Long-term MA: {long_term_ma[-1]}")
+        self.logger.info(f"Before Action - Capital: {self.model.capital}, Holding Quantity: {self.model.holding_quantity}, Average Price: {self.model.average_price}")
 
         if self.model.holding_quantity == 0:
-            quantity = (
-                                   self.model.capital // current_price) // 100 * 100  # 100株単位に調整
+            quantity = (self.model.capital // current_price) // 100 * 100  # 100株単位に調整
             if quantity > 0:
                 action = 'buy'
         else:
-            if current_price >= self.model.average_price * upper_limit and \
-                    short_term_ma[-1] > long_term_ma[-1]:
-                action, quantity = 'sell', (
-                                                       self.model.holding_quantity // 100) * 100  # 100株単位に調整
+            if current_price >= self.model.average_price * upper_limit and short_term_ma[-1] > long_term_ma[-1]:
+                action, quantity = 'sell', (self.model.holding_quantity // 100) * 100  # 100株単位に調整
             elif current_price <= self.model.average_price * lower_limit:
-                action, quantity = 'sell', (
-                                                       self.model.holding_quantity // 100) * 100  # 100株単位に調整
+                action, quantity = 'sell', (self.model.holding_quantity // 100) * 100  # 100株単位に調整
 
-        self.logger.info(
-            f"After Action - Capital: {self.model.capital}, Holding Quantity: {self.model.holding_quantity}, Average Price: {self.model.average_price}")
-        self.logger.info(
-            f"Action: {action}, Quantity: {quantity}, Price: {current_price}")
+        self.logger.info(f"After Action - Capital: {self.model.capital}, Holding Quantity: {self.model.holding_quantity}, Average Price: {self.model.average_price}")
+        self.logger.info(f"Action: {action}, Quantity: {quantity}, Price: {current_price}")
         return action, quantity
 
+# トレンドを判定する関数
+def determine_trend(prices):
+    trade_controller = TradeController(pd.DataFrame(prices), "", initial_capital)
+    short_term_ma = trade_controller.calculate_moving_average(prices, 5)[-1]  # 5日移動平均
+    long_term_ma = trade_controller.calculate_moving_average(prices, 10)[-1]  # 10日移動平均
+    last_close = prices.iloc[-1]  # 前日の終値を正しい方法で取得
+
+    if last_close > short_term_ma * 1.2:
+        return "上昇トレンド（前日高騰）"
+    elif short_term_ma > long_term_ma:
+        return "上昇トレンド"
+    else:
+        return "下降トレンド"
 
 def optimize_parameters(df, upper_limit, lower_limit, ticker_symbol):
     trade_controller = TradeController(df, ticker_symbol, initial_capital)
 
     for price in df['Close']:
         logging.info(f"Current price: {price}")
-        action, quantity = trade_controller.trading_logic(price, upper_limit,
-                                                          lower_limit)
+        action, quantity = trade_controller.trading_logic(price, upper_limit, lower_limit)
 
-        # action に基づいてトレードを実行
         if action == 'buy':
             trade_controller.model.buy_stock(price, quantity)
         elif action == 'sell':
             trade_controller.model.sell_stock(price, quantity)
 
-    final_value = trade_controller.model.capital + trade_controller.model.holding_quantity * \
-                  df.iloc[-1]['Close']
+    final_value = trade_controller.model.capital + trade_controller.model.holding_quantity * df.iloc[-1]['Close']
     profit_loss = final_value - initial_capital
     return final_value, profit_loss
-
 
 def main():
     for ticker_symbol in ticker_symbols:
         log_dir = setup_logging(ticker_symbol)
         stockdata_date_str = datetime.datetime.now().strftime('%Y%m%d')
-        csv_filename = os.path.join("stockdata",
-                                    f'{ticker_symbol.replace(".", "_")}_one_month_daily_stock_data_{stockdata_date_str}.csv')
+        csv_filename = os.path.join("stockdata", f'{ticker_symbol.replace(".", "_")}_one_month_daily_stock_data_{stockdata_date_str}.csv')
         df = load_stock_data(csv_filename)
 
-        param_combinations = [(ul, ll) for ul in upper_limits for ll in
-                              lower_limits]
+        # 最新の10営業日のデータのみを使用
+        df = df.tail(10)
+
+        param_combinations = [(ul, ll) for ul in upper_limits for ll in lower_limits]
 
         best_upper_limit, best_lower_limit = None, None
         best_profit_loss = float('-inf')
         results = []
 
         for upper_limit, lower_limit in param_combinations:
-            final_value, profit_loss = optimize_parameters(df, upper_limit,
-                                                           lower_limit,
-                                                           ticker_symbol)
+            final_value, profit_loss = optimize_parameters(df, upper_limit, lower_limit, ticker_symbol)
             results.append((upper_limit, lower_limit, final_value, profit_loss))
             if profit_loss > best_profit_loss:
                 best_upper_limit = upper_limit
                 best_lower_limit = lower_limit
                 best_profit_loss = profit_loss
 
-            logging.info(
-                f"Upper limit: {upper_limit}, Lower limit: {lower_limit}, Final value: {final_value}, Profit/Loss: {profit_loss}")
+            logging.info(f"Upper limit: {upper_limit}, Lower limit: {lower_limit}, Final value: {final_value}, Profit/Loss: {profit_loss}")
+
+        # トレンドの判定
+        trend = determine_trend(df['Close'])
 
         print(f"Ticker: {ticker_symbol}")
         print(f"Best upper limit: {best_upper_limit}")
         print(f"Best lower limit: {best_lower_limit}")
         print(f"Best Profit/Loss: {best_profit_loss}")
+        print(f"Current Trend: {trend}")
 
         # 結果をCSVに保存
-        results_df = pd.DataFrame(results,
-                                  columns=['upper_limit', 'lower_limit',
-                                           'final_value', 'profit_loss'])
-        results_filename = os.path.join(log_dir,
-                                        f'{ticker_symbol.replace(".", "_")}_optimal_parameters_results_{datetime.datetime.now().strftime("%Y%m%d%H%M")}.csv')
+        results_df = pd.DataFrame(results, columns=['upper_limit', 'lower_limit', 'final_value', 'profit_loss'])
+        results_filename = os.path.join(log_dir, f'{ticker_symbol.replace(".", "_")}_optimal_parameters_results_{datetime.datetime.now().strftime("%Y%m%d%H%M")}.csv')
         os.makedirs(os.path.dirname(results_filename), exist_ok=True)
         results_df.to_csv(results_filename, index=False)
         logging.info(f"Backtest results saved to {results_filename}")
